2abedf094d000b5a2d01101a089471913b669278
Tga123 <29838841+Tga123@users.noreply.github.com>
2021-09-12 21:24:17 +0300

    optimize and improve

--- LightSpellNamePlatePercent.lua
+++ LightSpellNamePlatePercent.lua
@@ -1,88 +1,132 @@
-LightSpellNamePlatePercent = LibStub("AceAddon-3.0"):NewAddon("LightSpellNamePlatePercent", "AceEvent-3.0", "AceTimer-3.0")
+--------------------INITIALIZE-------------------------------
+local LSNPP = LibStub("AceAddon-3.0"):NewAddon("LightSpellNamePlatePercent", "AceEvent-3.0")
 
-function LightSpellNamePlatePercent:OnEnable()
-	LightSpellNamePlatePercent.LSNPP_Timers = {};
-	LightSpellNamePlatePercent:RegisterEvent("NAME_PLATE_UNIT_ADDED",LSNPP_MainAdd)
-	LightSpellNamePlatePercent:RegisterEvent("NAME_PLATE_UNIT_REMOVED",LSNPP_MainRemove)
-	LightSpellNamePlatePercent:RegisterEvent("FORBIDDEN_NAME_PLATE_UNIT_ADDED",LSNPP_MainAdd)
-	LightSpellNamePlatePercent:RegisterEvent("FORBIDDEN_NAME_PLATE_UNIT_REMOVED",LSNPP_MainRemove)
+function LSNPP:OnEnable()
+	LSNPP:RegisterEvent("NAME_PLATE_UNIT_ADDED", "MainAdd")
+	LSNPP:RegisterEvent("NAME_PLATE_UNIT_REMOVED", "MainRemove")
+	LSNPP:RegisterEvent("FORBIDDEN_NAME_PLATE_UNIT_ADDED", "MainAdd")
+	LSNPP:RegisterEvent("FORBIDDEN_NAME_PLATE_UNIT_REMOVED", "MainRemove")
+	LSNPP:RegisterEvent("UNIT_MAXHEALTH")
+	LSNPP:RegisterEvent("UNIT_HEALTH")
+
+	LSNPP.HpMaxList = {}
+	LSNPP.MaxPower = 1
+	LSNPP.PowerType = 0
+	LSNPP.PowerToken = "MANA"
 end
-
-function LSNPP_FormatNumber (LSNPP_Number)
-	if (LSNPP_Number > 999999999) then
-		return format ("%.2fB", LSNPP_Number/1000000000)
-	elseif (LSNPP_Number > 999999) then
-		return format ("%.2fM", LSNPP_Number/1000000)
-	elseif (LSNPP_Number > 99999) then
-		return floor (LSNPP_Number/1000) .. "K"
-	elseif (LSNPP_Number > 999) then
-		return format ("%.1fK", (LSNPP_Number/1000))
+-----------------------------------------------------------
+-------------------------UTILITY---------------------------
+local function LSNPP_FormatNumber(Number)
+	if Number > 999999999 then
+		return format("%.2fB", Number / 1000000000)
+	elseif Number > 999999 then
+		return format("%.2fM", Number / 1000000)
+	elseif Number > 99999 then
+		return floor(Number / 1000) .. "K"
+	elseif Number > 999 then
+		return format("%.1fK", (Number / 1000))
 	end
-	return format ("%.0f", LSNPP_Number)
+	return format("%.0f", Number)
 end
 
-function LightSpellNamePlatePercent:LSNPP_UpdateFontStringsHP(LSNPP_UpdateHealthBar, LSNPP_unitID_UpdateHP)
-	local LSNPP_currentHealth, LSNPP_maxHealth = UnitHealth(LSNPP_unitID_UpdateHP), UnitHealthMax(LSNPP_unitID_UpdateHP)
-	LSNPP_UpdateHealthBar.LSNPP_Str_Health:SetText (LSNPP_FormatNumber(LSNPP_currentHealth) .. " (" .. ceil (LSNPP_currentHealth / LSNPP_maxHealth * 100) .. "%)")
-end
-
-function LightSpellNamePlatePercent:LSNPP_UpdateFontStringsMP(LSNPP_UpdateManaFrame, LSNPP_unitID_UpdateMP)
-	local LSNPP_currentPower, LSNPP_maxPower = UnitPower(LSNPP_unitID_UpdateMP), UnitPowerMax(LSNPP_unitID_UpdateMP)
-	LSNPP_UpdateManaFrame.LSNPP_Str_Power:SetText (LSNPP_FormatNumber(LSNPP_currentPower) .. " (" .. ceil (LSNPP_currentPower / LSNPP_maxPower * 100) .. "%)")
-end
-
-function LSNPP_MainAddHP(LSNPP_unitID)
-	local LSNPP_PlateFrame = C_NamePlate.GetNamePlateForUnit(LSNPP_unitID)
-	if LSNPP_PlateFrame == nil or not LSNPP_PlateFrame.UnitFrame or not LSNPP_PlateFrame.UnitFrame.healthBar then return end
-	local LSNPP_HealthBar = LSNPP_PlateFrame.UnitFrame.healthBar
-	if (not LSNPP_HealthBar.LSNPP_Str_Health) then	
-		LSNPP_HealthBar.LSNPP_Str_Health = LSNPP_HealthBar:CreateFontString (nil, "Overlay", "GameFontNormal")
-		LSNPP_HealthBar.LSNPP_Str_Health:SetPoint ("CENTER", LSNPP_HealthBar)
-		LSNPP_HealthBar.LSNPP_Str_Health:SetSize (LSNPP_HealthBar:GetSize())
-		LSNPP_HealthBar.LSNPP_Str_Health:SetTextColor(1, 1, 1, 1)
-		LSNPP_HealthBar.LSNPP_Str_Health:SetShadowColor(0, 0, 0, 1)
-		LSNPP_HealthBar.LSNPP_Str_Health:Hide()
-		--print("|r|cFF0099FFLightSpellNamePlatePercent:|r DRAWING " .. LSNPP_unitID)
+function LSNPP:MainAdd(_, UnitID)
+	LSNPP:AddHpString(nil, UnitID)
+	if UnitIsPlayer(UnitID) then
+		LSNPP:AddMpString()
+		LSNPP:UnregisterEvent("NAME_PLATE_UNIT_ADDED")
+		LSNPP:UnregisterEvent("FORBIDDEN_NAME_PLATE_UNIT_ADDED")
+		LSNPP:RegisterEvent("NAME_PLATE_UNIT_ADDED", "AddHpString")
+		LSNPP:RegisterEvent("FORBIDDEN_NAME_PLATE_UNIT_ADDED", "AddHpString")
 	end
-	LSNPP_HealthBar.LSNPP_Str_Health:SetText("")
-	LightSpellNamePlatePercent.LSNPP_Timers[LSNPP_unitID .. "__HP"] = LightSpellNamePlatePercent:ScheduleRepeatingTimer("LSNPP_UpdateFontStringsHP", 0.15, LSNPP_HealthBar, LSNPP_unitID)
-	LSNPP_HealthBar.LSNPP_Str_Health:Show()
 end
 
-function LSNPP_MainAddMP(LSNPP_unitID)
-	local LSNPP_PlateFrame = C_NamePlate.GetNamePlateForUnit(LSNPP_unitID)
-	local LSNPP_PlateSubFrame = { LSNPP_PlateFrame:GetChildren() }
-	local LSNPP_ManaFrame
-	for _, LSNPP_Child in ipairs(LSNPP_PlateSubFrame) do
-		if (LSNPP_Child:GetName() == "ClassNameplateManaBarFrame") then
-			LSNPP_ManaFrame = LSNPP_Child
+function LSNPP:MainRemove(_, UnitID)
+	LSNPP.HpMaxList[UnitID] = nil
+end
+-----------------------------------------------------------
+-------------------------HEALTH---------------------------
+
+function LSNPP:UNIT_MAXHEALTH(_, UnitID)
+	if LSNPP.HpMaxList[UnitID] then
+		LSNPP.HpMaxList[UnitID] = UnitHealthMax(UnitID)
+		LSNPP:UNIT_HEALTH(nil, UnitID)
+	end
+end
+
+function LSNPP:UNIT_HEALTH(_, UnitID)
+	if LSNPP.HpMaxList[UnitID] then
+		local HpStr = C_NamePlate.GetNamePlateForUnit(UnitID).UnitFrame.healthBar.LSNPP_Str_Health
+		local CurrentHealth = UnitHealth(UnitID)
+		HpStr:SetText(
+			LSNPP_FormatNumber(CurrentHealth) .. " (" .. ceil(CurrentHealth / LSNPP.HpMaxList[UnitID] * 100) .. "%)"
+		)
+	end
+end
+
+function LSNPP:AddHpString(_, UnitID)
+	local PlateFrame = C_NamePlate.GetNamePlateForUnit(UnitID)
+	if not PlateFrame or not PlateFrame.UnitFrame or not PlateFrame.UnitFrame.healthBar then
+		return
+	end
+	--print("|r|cFF0099FFLSNPP:|r DRAWING " .. UnitID)
+	local HealthBar = PlateFrame.UnitFrame.healthBar
+	if not HealthBar.LSNPP_Str_Health then
+		HealthBar.LSNPP_Str_Health = HealthBar:CreateFontString(nil, "Overlay", "GameFontNormal")
+		HealthBar.LSNPP_Str_Health:SetPoint("TOPLEFT", HealthBar)
+		HealthBar.LSNPP_Str_Health:SetPoint("BOTTOMRIGHT", HealthBar)
+		HealthBar.LSNPP_Str_Health:SetTextColor(1, 1, 1, 1)
+		HealthBar.LSNPP_Str_Health:SetShadowColor(0, 0, 0, 1)
+		HealthBar.LSNPP_Str_Health:Show()
+	end
+	HealthBar.LSNPP_Str_Health:SetText("")
+	LSNPP.HpMaxList[UnitID] = 1
+	LSNPP:UNIT_MAXHEALTH(nil, UnitID)
+	LSNPP:UNIT_HEALTH(nil, UnitID)
+end
+---------------------------------------------------------
+-------------------------POWER---------------------------
+function LSNPP:PLAYER_TALENT_UPDATE()
+	LSNPP.PowerType, LSNPP.PowerToken = UnitPowerType("player")
+	LSNPP:UNIT_MAXPOWER(nil, "player", LSNPP.PowerToken)
+end
+
+function LSNPP:UNIT_MAXPOWER(_, UnitID, PowerID)
+	if UnitID == "player" and PowerID == LSNPP.PowerToken then
+		LSNPP.MaxPower = UnitPowerMax("player", LSNPP.PowerType)
+	end
+end
+
+function LSNPP:UNIT_POWER_FREQUENT(_, UnitID, PowerID)
+	if UnitID == "player" and PowerID == LSNPP.PowerToken then
+		local CurrentPower = UnitPower("player", LSNPP.PowerType)
+		LSNPP_Str_Power:SetText(
+			LSNPP_FormatNumber(CurrentPower) .. " (" .. ceil(CurrentPower / LSNPP.MaxPower * 100) .. "%)"
+		)
+	end
+end
+
+function LSNPP:AddMpString()
+	local PlateSubFrames = { C_NamePlate.GetNamePlateForUnit("player"):GetChildren() }
+	local PowerFrame
+	for _, Child in ipairs(PlateSubFrames) do
+		if Child:GetName() == "ClassNameplateManaBarFrame" then
+			PowerFrame = Child
 		end
 	end
-	if not LSNPP_ManaFrame then return end
-	if (not LSNPP_ManaFrame.LSNPP_Str_Power) then
-		LSNPP_ManaFrame.LSNPP_Str_Power = LSNPP_ManaFrame:CreateFontString (nil, "Overlay", "GameFontNormal")
-		LSNPP_ManaFrame.LSNPP_Str_Power:SetPoint ("CENTER", LSNPP_ManaFrame)
-		LSNPP_ManaFrame.LSNPP_Str_Power:SetSize (LSNPP_ManaFrame:GetSize())
-		LSNPP_ManaFrame.LSNPP_Str_Power:SetTextColor(1, 1, 1, 1)
-		LSNPP_ManaFrame.LSNPP_Str_Power:SetShadowColor(0, 0, 0, 1)
-		LSNPP_ManaFrame.LSNPP_Str_Power:Hide()
+	if not PowerFrame then
+		return
 	end
-	LSNPP_ManaFrame.LSNPP_Str_Power:SetText("")
-	LightSpellNamePlatePercent.LSNPP_Timers[LSNPP_unitID .. "__MP"] = LightSpellNamePlatePercent:ScheduleRepeatingTimer("LSNPP_UpdateFontStringsMP", 0.15, LSNPP_ManaFrame, LSNPP_unitID)
-	LSNPP_ManaFrame.LSNPP_Str_Power:Show()
+	LSNPP:PLAYER_TALENT_UPDATE()
+	local PowerString = PowerFrame:CreateFontString("LSNPP_Str_Power", "Overlay", "GameFontNormal")
+	PowerString:SetPoint("TOPLEFT", PowerFrame)
+	PowerString:SetPoint("BOTTOMRIGHT", PowerFrame)
+	PowerString:SetTextColor(1, 1, 1, 1)
+	PowerString:SetShadowColor(0, 0, 0, 1)
+	PowerString:SetText("")
+	PowerString:Show()
+	LSNPP:UNIT_POWER_FREQUENT(nil, "player", LSNPP.PowerToken)
+	LSNPP:RegisterEvent("PLAYER_TALENT_UPDATE")
+	LSNPP:RegisterEvent("UNIT_MAXPOWER")
+	LSNPP:RegisterEvent("UNIT_POWER_FREQUENT")
 end
-
-function LSNPP_MainAdd(_, LSNPP_unitID)
-	LSNPP_MainAddHP(LSNPP_unitID);
-	if (UnitIsUnit("player", LSNPP_unitID)) then 
-		LSNPP_MainAddMP(LSNPP_unitID);
-	end;
-end
-
-function LSNPP_MainRemove(_, LSNPP_unitID)
-	LightSpellNamePlatePercent:CancelTimer(LightSpellNamePlatePercent.LSNPP_Timers[LSNPP_unitID .. "__HP"])
-	if (UnitIsUnit("player", LSNPP_unitID)) then 
-		LightSpellNamePlatePercent:CancelTimer(LightSpellNamePlatePercent.LSNPP_Timers[LSNPP_unitID .. "__MP"])
-	end;
-	--print("|r|cFF0099FFLightSpellNamePlatePercent:|r called MainRemove " .. LSNPP_unitID)
-end
+-----------------------------------------------------------
